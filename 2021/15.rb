708520862fd45ec6447e164eadb8b54a6261f46a
Kateba <niklas.hasselmeyer@online.de>
2021-12-15 10:58:46 +0100

    Improve 2021/15 runtime by using a priority list

--- 2021/15.rb
+++ 2021/15.rb
@@ -2,7 +2,7 @@ require_relative '../aoc_defaults'
 
 def part1
   puts 'Part 1:'
-  map = get_input true
+  map = get_input
 
   lowest_score = find_shortest_path(map)
 
@@ -39,45 +39,41 @@ def part2
 end
 
 def find_shortest_path(map)
-  next_moves = []
+  next_moves = PriorityQueue.new
   lowest_scores = {}
-  insert_points_for(0, 0, map, 0, next_moves, lowest_scores)
-  puts "distances estimated"
+  insert_next_point(1, 0, map, 0, next_moves, lowest_scores)
+  insert_next_point(0, 1, map, 0, next_moves, lowest_scores)
 
-  while true
-    next_move = next_moves.find { |x| x.present? }.pop
-    if (next_move[0] == map.size - 1 && next_move[1] == map[0].size - 1)
-      return next_move[2]
+  loop do
+    _, score_until_now, x, y = next_moves.pop
+    if (x == map.size - 1 && y == map[0].size - 1)
+      return score_until_now
     end
 
-    insert_points_for(next_move[0], next_move[1], map, next_move[2], next_moves, lowest_scores)
+    next if lowest_scores[[x, y]]&.< score_until_now
+    insert_next_point(x - 1, y, map, score_until_now, next_moves, lowest_scores)
+    insert_next_point(x, y - 1, map, score_until_now, next_moves, lowest_scores)
+    insert_next_point(x + 1, y, map, score_until_now, next_moves, lowest_scores)
+    insert_next_point(x, y + 1, map, score_until_now, next_moves, lowest_scores)
   end
-end
-
-def insert_points_for(x, y, map, score_until_now, next_moves, lowest_scores)
-  return if lowest_scores[[x, y]]&.< score_until_now
-  insert_next_point(x - 1, y, map, score_until_now, next_moves, lowest_scores)
-  insert_next_point(x, y - 1, map, score_until_now, next_moves, lowest_scores)
-  insert_next_point(x + 1, y, map, score_until_now, next_moves, lowest_scores)
-  insert_next_point(x, y + 1, map, score_until_now, next_moves, lowest_scores)
 end
 
 def insert_next_point(x, y, map, score_until_now, next_moves, lowest_scores)
   return if x < 0 || y < 0 || x >= map.size || y >= map[0].size
   score_until_next = score_until_now + map[x][y]
   score_min_until_end = get_distance_estimate(x, y, map) + score_until_now
-  unless lowest_scores[[x, y]]&.<= score_min_until_end
-    next_moves[score_min_until_end] ||= []
-    next_moves[score_min_until_end] << [x, y, score_until_next] if
-    lowest_scores[[x, y]] = score_min_until_end
+  unless lowest_scores[[x, y]]&.<= score_until_next
+    next_moves << [-score_min_until_end, score_until_next, x, y]
+    lowest_scores[[x, y]] = score_until_next
   end
 end
 
 def get_distance_estimate(x, y, map)
-  if map != @last_map
+  if map != @last_map # Memoisation resets when changing the map for part 2
     @last_map = map
     @distance_estimates = { [map.size - 1, map[0].size - 1] => map.last.last }
   end
+
   return 10 * map.size if x >= map.size || y >= map[0].size
   @distance_estimates[[x, y]] ||= [get_distance_estimate(x + 1, y, map), get_distance_estimate(x, y + 1, map)].min + map[x][y]
 end
@@ -102,6 +98,82 @@ def get_test_input(number)
   1293138521
   2311944581
   TEST
+end
+
+# https://gist.github.com/PaulSanchez/a68e2b40af587dc968615f73c446e1e5
+class PriorityQueue
+  attr_reader :elements
+
+  def initialize
+    clear
+  end
+
+  def <<(element)
+    @elements << element
+    bubble_up(@elements.size - 1)
+  end
+
+  alias push <<
+
+  def peek
+    @elements[1]
+  end
+
+  def pop
+    max = @elements[1]
+    bubble_down_and_replace(1)
+    max
+  end
+
+  def clear
+    @elements = [nil]
+  end
+
+  def empty?
+    @elements.length < 2
+  end
+
+  private
+
+  def bubble_up(index)
+    target = @elements[index]
+    loop do
+      parent_index = index / 2
+      if parent_index < 1 || (@elements[parent_index] <=> target) >= 0
+        @elements[index] = target
+        return
+      end
+
+      @elements[index] = @elements[parent_index]
+      index = parent_index
+    end
+  end
+
+  def bubble_down_and_replace(index)
+    target = @elements.pop
+
+    loop do
+      child_index = (index * 2)
+
+      if child_index > @elements.size - 1 && index < @elements.size
+        @elements[index] = target
+        return
+      end
+
+      not_the_last_element = child_index < @elements.size - 1
+      left_element = @elements[child_index]
+      right_element = @elements[child_index + 1]
+      child_index += 1 if not_the_last_element && (right_element <=> left_element) == 1
+
+      if (target <=> @elements[child_index]) >= 0
+        @elements[index] = target
+        return
+      end
+
+      @elements[index] = @elements[child_index]
+      index = child_index
+    end
+  end
 end
 
 if __FILE__ == $0
