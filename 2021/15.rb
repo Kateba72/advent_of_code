0c6fc630f43a4682a8d683df9d47626ebeb96900
Kateba <niklas.hasselmeyer@online.de>
2021-12-15 16:36:57 +0100

    Changed distance heuristic yet again, because the old one overestimated some distances

--- 2021/15.rb
+++ 2021/15.rb
@@ -2,7 +2,7 @@ require_relative '../aoc_defaults'
 
 def part1
   puts 'Part 1:'
-  map = get_input
+  map = get_input true
 
   lowest_score = find_shortest_path(map)
 
@@ -41,6 +41,7 @@ end
 def find_shortest_path(map)
   next_moves = PriorityQueue.new
   lowest_scores = {}
+  calculate_distance_estimates(map)
   insert_next_point(1, 0, map, 0, next_moves, lowest_scores)
   insert_next_point(0, 1, map, 0, next_moves, lowest_scores)
 
@@ -61,21 +62,53 @@ end
 def insert_next_point(x, y, map, score_until_now, next_moves, lowest_scores)
   return if x < 0 || y < 0 || x >= map.size || y >= map[0].size
   score_until_next = score_until_now + map[x][y]
-  score_min_until_end = get_distance_estimate(x, y, map) + score_until_now
+  score_min_until_end = @distance_estimates[x][y] + score_until_now
   unless lowest_scores[[x, y]]&.<= score_until_next
     next_moves << [-score_min_until_end, score_until_next, x, y]
     lowest_scores[[x, y]] = score_until_next
   end
 end
 
-def get_distance_estimate(x, y, map)
-  if map != @last_map # Memoisation resets when changing the map for part 2
-    @last_map = map
-    @distance_estimates = { [map.size - 1, map[0].size - 1] => map.last.last }
-  end
+def calculate_distance_estimates(map)
+  @distance_estimates = []
+  last_row = map.size - 1
+  last_column = map[0].size - 1
+  last_diagonal = last_row + last_column
 
-  return 10 * map.size if x >= map.size || y >= map[0].size
-  @distance_estimates[[x, y]] ||= [get_distance_estimate(x + 1, y, map), get_distance_estimate(x, y + 1, map)].min + map[x][y]
+  (0..last_row).each do |row|
+    @distance_estimates[row] = []
+  end
+  @distance_estimates[last_row][last_column] = map[last_row][last_column]
+
+  (0 .. last_diagonal-1).reverse_each do |diagonal|
+    diagonal_value = Float::INFINITY
+    column_range = [0, diagonal - last_row].max .. [last_column, diagonal].min
+
+    column_range.each do |column|
+      row = diagonal - column
+      # When we are on row R, column C in diagonal D=R+C, we either go to diagonal D+1
+      # or we go back, but then return later to diagonal D. Take Manhattan distance to
+      # that other point in diagonal D.
+      # All points left/down in the diagonal are considered now, the points up/right
+      # will be considered in the next loop
+      this_point_estimate = @distance_estimates[row][column] = [
+        @distance_estimates[row][column + 1] || Float::INFINITY,
+        @distance_estimates[row + 1]&.at(column) || Float::INFINITY,
+        diagonal_value + 2
+      ].min + map[row][column]
+      diagonal_value = diagonal_value + 2 < this_point_estimate ? diagonal_value + 2 : this_point_estimate
+    end
+
+    diagonal_value = Float::INFINITY
+    column_range.reverse_each do |column|
+      row = diagonal - column
+      this_point_estimate = @distance_estimates[row][column] = [
+        @distance_estimates[row][column],
+        diagonal_value + 2 + map[row][column]
+      ].min
+      diagonal_value = diagonal_value + 2 < this_point_estimate ? diagonal_value + 2 : this_point_estimate
+    end
+  end
 end
 
 def get_input(test=false)
