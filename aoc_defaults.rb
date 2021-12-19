10075bf1fa676ce3d1b274940523e989b7ab9413
Kateba <niklas.hasselmeyer@online.de>
2021-12-19 10:57:26 +0100

    Solved 2021/19

--- aoc_defaults.rb
+++ aoc_defaults.rb
@@ -40,3 +40,19 @@ end
 class Array
   prepend ArrayMixins
 end
+
+module VectorMixins
+  def p_norm_to_p(v, p)
+    Vector.Raise ErrDimensionMismatch if size != v.size
+
+    d = 0
+    each2(v) do |v1, v2|
+      d += (v1 - v2).abs ** p
+    end
+    d
+  end
+end
+
+class Vector
+  prepend VectorMixins
+end
