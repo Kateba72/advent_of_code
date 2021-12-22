bd54d48b99d3ab2b0b26bde0bbe0d632daadc55d
Kateba <niklas.hasselmeyer@online.de>
2021-12-22 08:28:19 +0100

    Solved 2021 20-22

--- aoc_defaults.rb
+++ aoc_defaults.rb
@@ -56,3 +56,24 @@ end
 class Vector
   prepend VectorMixins
 end
+
+module RangeMixins
+  def intersection(other)
+    return 0...0 unless self.max && other.max # Ensure both ranges are not empty
+    return 0...0 if self.max < other.min || other.max < self.min
+    [self.min, other.min].max .. [self.max, other.max].min
+  end
+  alias_method :&, :intersection
+
+  def union(other)
+    return self unless other.max
+    return other unless self.max # Deal with empty ranges
+    raise ArgumentError("Intersection of disjunct ranges") if self.max < other.min - 1 || other.max < self.min - 1
+    [self.min, other.min].min .. [self.max, other.max].max
+  end
+  alias_method :|, :union
+end
+
+class Range
+  prepend RangeMixins
+end
