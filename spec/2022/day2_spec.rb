5004d69f21f49e05ed824fbe24650891bced0993
Niklas Hasselmeyer <kateba@posteo.de>
2022-12-02 06:45:43 +0100

    Solve 2022-02

---
+++ spec/2022/day2_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../../2022/day2'
+
+RSpec.describe Day2 do
+  TEST_INPUT = <<~TEST_INPUT
+    A Y
+    B X
+    C Z
+  TEST_INPUT
+
+  subject { described_class.new(test_input: TEST_INPUT) }
+
+  describe '#part1' do
+    it 'gets the correct result for the test input' do
+      expect(subject.part1).to eq 15
+    end
+  end
+
+  describe '#part2' do
+    it 'gets the correct result for the test input' do
+      expect(subject.part2).to eq 12
+    end
+  end
+end
