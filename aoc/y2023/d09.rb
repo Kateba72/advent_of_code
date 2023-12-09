7244761158435995d7b38c002f3ab781ad1e6ace
Niklas Hasselmeyer <kateba@posteo.de>
2023-12-09 06:25:09 +0100

    Solve 2023-09

---
+++ aoc/y2023/d09.rb
@@ -0,0 +1,77 @@
+# require 'matrix'
+require_relative '../base_class'
+
+module AoC
+  module Y2023
+    class D09 < BaseClass
+
+      def part1
+        input = get_input
+
+        input.sum do |line|
+          interpolated = interpolate(line)
+          extrapolate_next(interpolated)
+        end
+      end
+
+      memoize def interpolate(line)
+        interpolated = [line]
+        until line.all? { |x| x.zero? }
+          interpolated << line[1..].map.with_index do |value, index|
+            value - line[index]
+          end
+          line = interpolated.last
+        end
+        interpolated
+      end
+
+      def extrapolate_next(interpolated)
+        interpolated.sum do |line|
+          line.last
+        end
+      end
+
+      def extrapolate_previous(interpolated)
+        interpolated.map.with_index do |value, index|
+          value.first * (index % 2 == 0 ? 1 : -1)
+        end.sum
+      end
+
+      def part2
+        input = get_input
+
+        input.sum do |line|
+          interpolated = interpolate(line)
+          extrapolate_previous(interpolated)
+        end
+      end
+
+      def initialize(test: false, test_input: nil)
+        @test = test
+        @test_input = test_input
+      end
+
+      private
+
+      def get_input
+        super.split("\n").map do |line|
+          line.split(' ').map &:to_i
+        end
+      end
+
+      def get_test_input(number)
+        <<~TEST
+          10 13 16 21 30 45
+        TEST
+      end
+
+      AOC_YEAR = 2023
+      AOC_DAY = 9
+    end
+  end
+end
+
+if __FILE__ == $0
+  today = AoC::Y2023::D09.new(test: false)
+  today.run
+end
