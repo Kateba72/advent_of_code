652a2952e55ef4e3bb0a78b60a481d22252a994a
Niklas Hasselmeyer <kateba@posteo.de>
2023-12-15 11:42:33 +0100

    Solve 2023-14 and 2023-15

---
+++ aoc/y2023/d14.rb
@@ -0,0 +1,111 @@
+# require 'matrix'
+require_relative '../base_class'
+
+module AoC
+  module Y2023
+    class D14 < BaseClass
+
+      def part1
+        rows = get_input
+        cols = (0...rows[0].size).map do |i|
+          line = rows.map { |r| r[i] }.join
+          line.gsub /\.+O[.O]*/ do |moving|
+            rocks = moving.count 'O'
+            'O' * rocks + '.' * (moving.size - rocks)
+          end
+        end
+        calc_load(cols)
+      end
+
+      def calc_load(arrangement)
+        load = 0
+        arrangement.each do |col|
+          next_moving = col.index('O')
+          while next_moving
+            load += col.size - next_moving
+
+            next_moving = col.index('O', next_moving + 1)
+          end
+        end
+        load
+      end
+
+      def part2
+        rows = get_input
+
+        arrangement = (0...rows[0].size).map do |i|
+          rows.map { |r| r[i] }.join
+        end
+
+        previous_spins = { arrangement => 0 }
+        spins_reverse = { 0 => arrangement }
+
+        final_arrangement = (1..).each do |i|
+          new_arrangement = do_spin(arrangement)
+          if previous_spins.has_key? new_arrangement
+            loop_start = previous_spins[new_arrangement]
+            loop_size = i - loop_start
+            step = (1000000000 - loop_start) % loop_size + loop_start
+            break spins_reverse[step]
+          else
+            previous_spins[new_arrangement] = i
+            spins_reverse[i] = new_arrangement
+          end
+          arrangement = new_arrangement
+        end
+
+        calc_load final_arrangement
+      end
+
+      def do_spin(arrangement)
+        4.times do
+          rows = arrangement.map do |line|
+            line.gsub /\.+O[.O]*/ do |moving|
+              rocks = moving.count 'O'
+              'O' * rocks + '.' * (moving.size - rocks)
+            end
+          end
+          cols = (0...rows[0].size).map do |i|
+            rows.map { |r| r[i] }.join
+          end.reverse
+          arrangement = cols
+        end
+        arrangement
+      end
+
+      def initialize(test: false, test_input: nil)
+        @test = test
+        @test_input = test_input
+      end
+
+      private
+
+      def get_input
+        super.split("\n")
+      end
+
+      def get_test_input(number)
+        <<~TEST
+          O....#....
+          O.OO#....#
+          .....##...
+          OO.#O....O
+          .O.....O#.
+          O.#..O.#.#
+          ..O..#O..O
+          .......O..
+          #....###..
+          #OO..#....
+        TEST
+      end
+
+      AOC_YEAR = 2023
+      AOC_DAY = 14
+    end
+  end
+end
+
+if __FILE__ == $0
+  today = AoC::Y2023::D14.new(test: false)
+  today.run
+end
---
+++ aoc/y2023/d15.rb
@@ -0,0 +1,79 @@
+# require 'matrix'
+require_relative '../base_class'
+
+module AoC
+  module Y2023
+    class D15 < BaseClass
+
+      def part1
+        input = get_input
+        input.sum { |x| hash(x) }
+      end
+
+      def part2
+        input = get_input
+
+        boxes = Array.new(256) { {} }
+
+        input.each_with_index do |step, index|
+          match = step.match /[-=]/
+          label = match.pre_match
+          box = hash(label)
+          if match.to_s == '='
+            focal = match.post_match.to_i
+            if boxes[box].has_key? label
+              boxes[box][label].focal = focal
+            else
+              boxes[box][label] = Lens.new(focal, index)
+            end
+          else
+            boxes[box].delete(label)
+          end
+        end
+
+        boxes.map.with_index do |box, box_index|
+          lenses = box.values.sort_by { |lens| lens.insertion_time }
+          lenses.map.with_index do |lens, lens_index|
+            (box_index + 1) * (lens_index + 1) * (lens.focal)
+          end.sum
+        end.sum
+      end
+
+      Lens = Struct.new(:focal, :insertion_time)
+
+      def hash(str)
+        value = 0
+        str.chars.each do |ch|
+          value += ch.ord
+          value *= 17
+          value %= 256
+        end
+        value
+      end
+
+      def initialize(test: false, test_input: nil)
+        @test = test
+        @test_input = test_input
+      end
+
+      private
+
+      def get_input
+        super.strip.split(',')
+      end
+
+      def get_test_input(number)
+        <<~TEST
+        TEST
+      end
+
+      AOC_YEAR = 2023
+      AOC_DAY = 15
+    end
+  end
+end
+
+if __FILE__ == $0
+  today = AoC::Y2023::D15.new(test: false)
+  today.run
+end
