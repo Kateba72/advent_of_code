ec7a3ecc540d5982c830ae9d6e824bb4dbbe99bb
Niklas Hasselmeyer <kateba@posteo.de>
2024-12-04 06:19:52 +0100

    Solve 2024-04

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ I'm Niklas, a rubyist, and I solve Advent of Code each december. Here are my sol
 
 <!-- AOC TILES BEGIN -->
 <h1 align="center">
-  Advent of Code - 179/456 ⭐
+  Advent of Code - 181/458 ⭐
 </h1>
 <h1 align="center">
-  2024 - 6 ⭐ - Ruby
+  2024 - 8 ⭐ - Ruby
 </h1>
 <a href="aoc/y2024/d01.rb">
   <img src=".aoc_tiles/tiles/2024/01.png" width="161px">
@@ -17,6 +17,9 @@ I'm Niklas, a rubyist, and I solve Advent of Code each december. Here are my sol
 </a>
 <a href="aoc/y2024/d03.rb">
   <img src=".aoc_tiles/tiles/2024/03.png" width="161px">
+</a>
+<a href="aoc/y2024/d04.rb">
+  <img src=".aoc_tiles/tiles/2024/04.png" width="161px">
 </a>
 <h1 align="center">
   2023 - 50 ⭐ - Ruby
---
+++ aoc/y2024/d04.rb
@@ -0,0 +1,70 @@
+require_relative '../solution'
+
+module AoC
+  module Y2024
+    class D04 < Solution
+
+      def part1
+        input = parse_input
+        input.with_coords.sum do |letter, pos|
+          next 0 unless letter == 'X'
+
+          Grid2d::NEIGHBORS_WITH_DIAGONALS.count do |dir|
+            input.at(pos+dir) == 'M' && input.at(pos+2*dir) == 'A' && input.at(pos+3*dir) == 'S'
+          end
+        end
+      end
+
+      def part2
+        input = parse_input
+
+        diagonals = (Grid2d::NEIGHBORS_WITH_DIAGONALS - Grid2d::NEIGHBORS).map do |dir|
+          rot = Vector[dir[1], -dir[0]]
+          [dir, rot]
+        end
+
+        input.with_coords.sum do |letter, pos|
+          next 0 unless letter == 'A'
+
+          diagonals.count do |dir, rot|
+            input.at(pos-dir) == 'M' && input.at(pos+dir) == 'S' && input.at(pos-rot) == 'M' && input.at(pos+rot) == 'S'
+          end
+        end
+      end
+
+      def initialize(test: false, test_input: nil)
+        @test = test
+        @test_input = test_input
+      end
+
+      private
+
+      memoize def parse_input
+        Grid2d.from_string(get_input)
+      end
+
+      def get_test_input(number)
+        <<~TEST
+          MMMSXXMASM
+          MSAMXMSMSA
+          AMXSXMAAMM
+          MSAMASMSMX
+          XMASAMXAMM
+          XXAMMXXAMA
+          SMSMSASXSS
+          SAXAMASAAA
+          MAMMMXMMMM
+          MXMXAXMASX
+        TEST
+      end
+
+      AOC_YEAR = 2024
+      AOC_DAY = 4
+    end
+  end
+end
+
+if __FILE__ == $0
+  today = AoC::Y2024::D04.new(test: false)
+  today.run
+end
